f2a444302bea51cdd27324843d72034bcc9d6301
MarvMan <marvin.kai@gmx.de>
2018-03-21 20:38:15 +0100

    Load templates

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ require 'inc/classes/Settings.class.inc.php';
 $db = new Mysql(DATABASE_HOST, DATABASE_DATABASE, DATABASE_USER, DATABASE_PASSWORD);
 $db->Connect(DATABASE_HOST, DATABASE_DATABASE, DATABASE_USER, DATABASE_PASSWORD);
 $smarty = new \Smarty();
-echo '<b>Test-Value:</b>&nbsp;'.Settings::getSetting("test");
+$smarty->caching = false;
+$smarty->setTemplateDir("cm_content/themes/modern/templates");
+$smarty->assign("foo", "bar");
+echo($smarty->fetch("page.tpl"));
 
 ?>
---
+++ templates_c/cffc59861612dec4cf7a9b18fcfe75c0a18f06cb_0.file.page.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 3.1.31, created on 2018-03-21 20:37:51
+  from "/Applications/XAMPP/xamppfiles/htdocs/contentmonkey/cm_content/themes/modern/templates/page.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5ab2b48f7cceb8_05841410',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'cffc59861612dec4cf7a9b18fcfe75c0a18f06cb' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/contentmonkey/cm_content/themes/modern/templates/page.tpl',
+      1 => 1521661045,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ab2b48f7cceb8_05841410 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><?php echo $_smarty_tpl->tpl_vars['foo']->value;?>
+</h1>
+</body>
+</html>
+<?php }
+}
